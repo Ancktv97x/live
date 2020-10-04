f07366dbed6ca15c99b3a0ca013788cad73c9ead
Ancktv97x <wapvui9x@gmail.com>
2020-10-05 00:07:21 +0700

    Add files via upload

---
+++ install-VNC-Server-CentOS-7-RutGon.sh
@@ -0,0 +1,8 @@
+yum install tigervnc-server
+yum groupinstall "GNOME Desktop"
+vncpasswd /
+firewall-cmd --permanent --zone=public --add-service vnc-server
+firewall-cmd --reload
+systemctl daemon-reload
+systemctl enable vncserver@:1.service
+vncserver
